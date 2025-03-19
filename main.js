2829ad6b1595be9c938a158029adf46d28bec4aa
Abir Tasrif Anto <abirtasrif@gmail.com>
2025-03-19 11:58:51 +0600

    ready

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true,
+      webSecurity: true,
+      javascript: true,
+      webviewTag: false,
+    },
+  });
+
+  // Set a custom user agent
+  // mainWindow.webContents.setUserAgent(
+  //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+  // );
+
+  mainWindow.loadURL("https://mail.google.com/chat");
+
+  // Open DevTools
+  // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
